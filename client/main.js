9530b3b9e47680e3bfe5b0d7234efe6816376861
Joe Venton <joeventon@TechEd-0028.local>
2024-06-14 10:04:26 +0100

    updated urls for render deployment

--- client/main.js
+++ client/main.js
@@ -1,5 +1,20 @@
 const form = document.getElementById("messageForm");
 
+async function fetchAndRenderRamen() {
+  const response = await fetch("https://renderdemo-3ohl.onrender.com/ramen");
+  const ramenList = await response.json();
+  const ramenListDiv = document.getElementById("ramenList");
+  ramenListDiv.innerHTML = "";
+
+  ramenList.forEach((ramen) => {
+    const ramenDiv = document.createElement("div");
+    ramenDiv.innerHTML = `<p>ID: ${ramen.id}, Flavour: ${ramen.flavour}, Price: ${ramen.price}, Spiciness: ${ramen.spiciness} Time To Cook: ${ramen.time_to_cook}</p>`;
+    ramenListDiv.appendChild(ramenDiv);
+  });
+}
+
+fetchAndRenderRamen();
+
 form.addEventListener("submit", submitButton);
 
 function submitButton(event) {
@@ -7,7 +22,7 @@ function submitButton(event) {
 
   const formData = new FormData(form);
   const formValues = Object.fromEntries(formData);
-  fetch("http://localhost:8080/ramen", {
+  fetch("https://renderdemo-3ohl.onrender.com/ramen", {
     method: "GET",
     headers: {
       "content-type": "application/json",
